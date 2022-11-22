4cd8f9b2e14b0d9258f78c392fd38dd23109a2a6
zrtch <1160639597@qq.com>
2022-11-22 10:52:41 +0800

    获取网络API数据

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React from 'react'
 import logo from './assets/images/logo.svg'
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 import styles from './App.module.css'
 
-function App() {
+interface Props{
+
+}
+
+interface State{
+  robotGallery: any[]
+}
+class App extends React.Component<Props, State> {
+  constructor(props: Props){
+    super(props);
+    this.state = {
+      robotGallery: []
+    }
+  }
+
+  componentDidMount()   {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then((data) => this.setState({ robotGallery: data }) )
+  }
+
   // 循环输出 Robot 组件
-  return (
-    <div className={styles.app}>
+  render(): React.ReactNode {
+    return (
+      <div className={styles.app}>
         <div className={styles.appHeader}>
-          <img src={logo} alt="" className={styles.appLogo}/>
+          <img src={logo} alt="" className={styles.appLogo} />
           <h1>Robot 瞅你咋地</h1>
         </div>
         <ShoppingCart />
-       <div className={styles.robotList}>
-        {robots.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => (
+            <Robot id={r.id} email={r.email} name={r.name} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
--- robot-gallery/src/components/ShoppingCart.tsx
+++ robot-gallery/src/components/ShoppingCart.tsx
@@ -1,5 +1,6 @@
 import React  from "react";
 import styles from './ShoppingCart.module.css'
+import { FiShoppingCart } from 'react-icons/fi'
 
 interface Props{
 
@@ -15,17 +16,26 @@ class ShoppingCart extends React.Component<Props, State>{ //继承React.Componen
       this.state = {
         isOpen: false
       }
+      this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e.target)
+    console.log(e.currentTarget)
+    //点击span元素下拉菜单正常，点击icon就无效
+    if((e.target as HTMLElement).nodeName === 'SPAN'){
+      this.setState({ isOpen: !this.state.isOpen })
+    }
   }
 
   render(){
      return(
       <div className={styles.cartContainer}>
-        <button className={styles.button} onClick={()=>{
-          this.setState({
-            isOpen: !this.state.isOpen
-          })
-        }}
-        >购物车 2 (件)</button>
+        <button className={styles.button} onClick={this.handleClick}
+        >
+          <FiShoppingCart />
+          <span> 购物车 2 (件)</span>
+         </button>
         <div className={styles.cartDropDown}
           style={{display: this.state.isOpen ? 'block' : 'none'}}
         >
