0cd6147dd254809323c727adf7118f521f938c65
zrtch <1160639597@qq.com>
2022-11-28 10:41:46 +0800

    feat:组件拆分

--- travel-demo/src/App.tsx
+++ travel-demo/src/App.tsx
@@ -1,73 +1,12 @@
 import React from 'react'
-import logo from './logo.svg'
 import styles from './App.module.css'
-import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd'
-import { GlobalOutlined } from '@ant-design/icons'
-
-const { Header, Footer } = Layout
+import { Header, Footer } from './components' //整个组件库 components 成为一个整体，把整个组价库拆分出来，以独立项目依赖的形式，提供别人使用
 
 function App() {
   return (
     <div className={styles.App}>
-      <Layout>
-        <div className={styles['app-header']}>
-          {/* top-header */}
-          <div className={styles['top-header']}>
-            <div className={styles.inner}>
-              <Typography.Text>让旅游更幸福</Typography.Text>
-              <Dropdown.Button
-                style={{ marginLeft: 15, display: 'inline' }}
-                overlay={
-                  <Menu>
-                    <Menu.Item>中文</Menu.Item>
-                    <Menu.Item>English</Menu.Item>
-                  </Menu>
-                }
-                icon={<GlobalOutlined />}
-              >
-                语言
-              </Dropdown.Button>
-              <Button.Group className={styles['button-group']}>
-                <Button>注册</Button>
-                <Button>登陆</Button>
-              </Button.Group>
-            </div>
-          </div>
-          <Header className={styles['main-header']}>
-            <img src={logo} alt="logo" className={styles['App-logo']} />
-            <Typography.Title level={3} className={styles.title}>
-              React旅游网
-            </Typography.Title>
-            <Input.Search
-              placeholder={'请输入旅游目的地、主题、或关键字'}
-              className={styles['search-input']}
-            />
-          </Header>
-          <Menu mode={'horizontal'} className={styles['main-menu']}>
-            <Menu.Item key={1}>旅游首页</Menu.Item>
-            <Menu.Item key={2}>周末游</Menu.Item>
-            <Menu.Item key={3}>跟团游</Menu.Item>
-            <Menu.Item key="4"> 自由行 </Menu.Item>
-            <Menu.Item key="5"> 私家团 </Menu.Item>
-            <Menu.Item key="6"> 邮轮 </Menu.Item>
-            <Menu.Item key="7"> 酒店+景点 </Menu.Item>
-            <Menu.Item key="8"> 当地玩乐 </Menu.Item>
-            <Menu.Item key="9"> 主题游 </Menu.Item>
-            <Menu.Item key="10"> 定制游 </Menu.Item>
-            <Menu.Item key="11"> 游学 </Menu.Item>
-            <Menu.Item key="12"> 签证 </Menu.Item>
-            <Menu.Item key="13"> 企业游 </Menu.Item>
-            <Menu.Item key="14"> 高端游 </Menu.Item>
-            <Menu.Item key="15"> 爱玩户外 </Menu.Item>
-            <Menu.Item key="16"> 保险 </Menu.Item>
-          </Menu>
-        </div>
-        <Footer>
-          <Typography.Title level={3} style={{ textAlign: 'center' }}>
-            版权所有 @ React 旅游网
-          </Typography.Title>
-        </Footer>
-      </Layout>
+      <Header />
+      <Footer />
     </div>
   )
 }
---
+++ travel-demo/src/components/header/Header.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styles from './Header.module.css'
+import logo from '../../assets/logo.svg'
+import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd'
+import { GlobalOutlined } from '@ant-design/icons'
+
+export const Header: React.FC = () => {
+  return (
+    <div className={styles.App}>
+      <div className={styles['app-header']}>
+        {/* top-header */}
+        <div className={styles['top-header']}>
+          <div className={styles.inner}>
+            <Typography.Text>让旅游更幸福</Typography.Text>
+            <Dropdown.Button
+              style={{ marginLeft: 15, display: 'inline' }}
+              overlay={
+                <Menu>
+                  <Menu.Item>中文</Menu.Item>
+                  <Menu.Item>English</Menu.Item>
+                </Menu>
+              }
+              icon={<GlobalOutlined />}
+            >
+              语言
+            </Dropdown.Button>
+            <Button.Group className={styles['button-group']}>
+              <Button>注册</Button>
+              <Button>登陆</Button>
+            </Button.Group>
+          </div>
+        </div>
+        <Layout.Header className={styles['main-header']}>
+          <img src={logo} alt="logo" className={styles['App-logo']} />
+          <Typography.Title level={3} className={styles.title}>
+            React旅游网
+          </Typography.Title>
+          <Input.Search
+            placeholder={'请输入旅游目的地、主题、或关键字'}
+            className={styles['search-input']}
+          />
+        </Layout.Header>
+        <Menu mode={'horizontal'} className={styles['main-menu']}>
+          <Menu.Item key={1}>旅游首页</Menu.Item>
+          <Menu.Item key={2}>周末游</Menu.Item>
+          <Menu.Item key={3}>跟团游</Menu.Item>
+          <Menu.Item key="4"> 自由行 </Menu.Item>
+          <Menu.Item key="5"> 私家团 </Menu.Item>
+          <Menu.Item key="6"> 邮轮 </Menu.Item>
+          <Menu.Item key="7"> 酒店+景点 </Menu.Item>
+          <Menu.Item key="8"> 当地玩乐 </Menu.Item>
+          <Menu.Item key="9"> 主题游 </Menu.Item>
+          <Menu.Item key="10"> 定制游 </Menu.Item>
+          <Menu.Item key="11"> 游学 </Menu.Item>
+          <Menu.Item key="12"> 签证 </Menu.Item>
+          <Menu.Item key="13"> 企业游 </Menu.Item>
+          <Menu.Item key="14"> 高端游 </Menu.Item>
+          <Menu.Item key="15"> 爱玩户外 </Menu.Item>
+          <Menu.Item key="16"> 保险 </Menu.Item>
+        </Menu>
+      </div>
+    </div>
+  )
+}
